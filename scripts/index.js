0080b51a5c95879b2e7ae8e41dfd0b093dbb1e5b
JacobEiserloh <jacobeiserloh@gmail.com>
2025-11-09 10:01:38 -0800

    added array

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: "El Capitan",
+    link: "https://images.unsplash.com/photo-1498429089284-41f8cf3ffd39?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070",
+  },
+  {
+    name: "Half Dome",
+    link: "https://plus.unsplash.com/premium_photo-1673603988651-99f79e4ae7d3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
+  },
+  {
+    name: "Birmingham",
+    link: "https://images.unsplash.com/photo-1440582096070-fa5961d9d682?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2076",
+  },
+  {
+    name: "Doodle Bug",
+    link: "https://images.unsplash.com/photo-1511532514522-a14584b0ad3b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1074",
+  },
+  {
+    name: "Ninja 300",
+    link: "https://images.unsplash.com/photo-1619441159429-352f81bddca2?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687",
+  },
+  {
+    name: "Web Design",
+    link: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1169",
+  },
+];
+
 // selecting edit profile elements
 const profileEditBtn = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
@@ -19,7 +46,7 @@ const addPostForm = newPostModal.querySelector(".modal__form");
 const captionInput = newPostModal.querySelector("#card-caption-input");
 const imageLinkInput = newPostModal.querySelector("#card-image-input");
 
-// functions
+// modal functions
 function handleEditSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = profileNameInput.value;
@@ -66,3 +93,7 @@ newPostCloseBtn.addEventListener("click", function () {
 });
 
 addPostForm.addEventListener("submit", handleAddPostSubmit);
+
+initialCards.forEach(function (card) {
+  console.log(card.name);
+});
